e9d12f6a5d29471aa0b8f499536caf20650af905
Subham Das <dassubham919@gmail.com>
2024-09-07 15:06:41 +0545

    Typecast

--- C++ with DSA/Variables and Data Types/Typecasting.cpp
+++ C++ with DSA/Variables and Data Types/Typecasting.cpp
@@ -11,7 +11,12 @@ int main (){
         //EXPLICIT CONVERSION
     cout << char('A') << "\n";
     cout << int('A') << "\n";
-    cout << ((float)10/3);
+    cout << ((float)10/3) << "\n";;
 
+    cout << bool(3)+2 << "\n";  // 3 because any value of bool other than 0(FALSE) becomes 1(TRUE)
+                            // so, bool(3)=1;   1+2=3
+
+    cout << (23.5 + 2 + 'A') << "\n";   // greatest is double or float. So, all other changes to double or float
+                                    // Ans- 90.5
     return 0;
 }

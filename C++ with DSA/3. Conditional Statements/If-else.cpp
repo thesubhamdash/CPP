fa3ae2c31dd23d793871cf3420c6bef65cb07ea6
Subham Das <dassubham919@gmail.com>
2024-09-07 22:29:30 +0545

    conditional

---
+++ C++ with DSA/3. Conditional Statements/If-else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    if (10>5){
+        cout << "Happy" << endl;
+    }
+    else if (10 == 10) {
+        cout << "Very Happy" << endl;
+    }
+    else{
+        cout << "SAD";
+    }
+    return 0;
+}

0043e72846fdc518e4ea87cb457d2b8e31878185
Subham Das <dassubham919@gmail.com>
2024-09-07 21:19:43 +0545

    Logical Operator

---
+++ C++ with DSA/2. Operators/LogicalO.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    cout << ((10 > 5) && (10 > 6)) << endl; 
+
+    cout << ((10 > 5) || (10 < 6)) << endl; 
+    
+    cout << (!(10 > 5)) << endl; 
+    
+    return 0;
+}

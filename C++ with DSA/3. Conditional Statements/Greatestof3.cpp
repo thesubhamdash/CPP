3757813ba5d454dd6029450870497e58b66d27bb
Subham Das <dassubham919@gmail.com>
2024-09-07 23:40:34 +0545

    Greatest of 3

---
+++ C++ with DSA/3. Conditional Statements/Greatestof3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int a, b, c;
+
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    if (a>b && a>c) {
+        cout << a << " is greatest";
+    }
+
+    else if (a<b && b>c) {
+        cout << b << " is greatest";
+    }
+
+    else {
+        cout << c << " is greatest";
+    }
+    return 0;
+}

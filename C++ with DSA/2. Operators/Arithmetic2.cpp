a05d8e8106695bda6628d77c8c023806e07df1f4
Subham Das <dassubham919@gmail.com>
2024-09-07 17:03:34 +0545

    Unary Operator

---
+++ C++ with DSA/2. Operators/Arithmetic2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Unary Operators" << endl;
+    int a = 1;
+    // a = a + 1;
+    cout << a++ << endl;
+
+    int b=5;
+    b=b-1;
+    cout << b-- << endl;
+}

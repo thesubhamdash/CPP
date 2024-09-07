aab7b017e21ce4886e58dd5cbd903a1ac5445f66
Subham Das <dassubham919@gmail.com>
2024-09-07 17:45:41 +0545

    Unary

--- C++ with DSA/2. Operators/Arithmetic2.cpp
+++ C++ with DSA/2. Operators/Arithmetic2.cpp
@@ -3,11 +3,27 @@ using namespace std;
 
 int main() {
     cout << "Unary Operators" << endl;
-    int a = 1;
+    int a = 5;
     // a = a + 1;
-    cout << a++ << endl;
+    // a++;
+    // cout << a << endl;
+    // a--;
+    // cout << a << endl;
 
-    int b=5;
-    b=b-1;
-    cout << b-- << endl;
+    /*POST INCREMENT OPERATOR "a++"
+        First the value is used and then increment is done.
+    */
+
+    int b = a++;    
+    cout << "a=" << a << endl;    
+    cout << "b=" << b << endl;
+
+    /*PRE INCREMENT OPERATOR "a++"
+        First the increment is done and then new value is used.
+    */
+    b = ++a;
+    cout << "a=" << a << endl;    
+    cout << "b=" << b << endl;
+    
+    return 0;
 }

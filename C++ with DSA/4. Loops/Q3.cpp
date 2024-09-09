000ce082b890baf818ce895704bd367713be8448
Subham Das <dassubham919@gmail.com>
2024-09-09 16:23:12 +0545

    Natural nums

---
+++ C++ with DSA/4. Loops/Q3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    for (i=1; i<=n; i++){
+        cout << i << endl;
+    }
+    return 0;
+}

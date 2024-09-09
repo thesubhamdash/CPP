d9112049b9a6ad5a7ce4e7063341dcb1d602e191
Subham Das <dassubham919@gmail.com>
2024-09-09 16:38:57 +0545

    For Loop Question

---
+++ C++ with DSA/4. Loops/Q6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    for (i=n; i>=1; i--){
+        cout << i << " ";
+    }  
+    cout << endl;
+}

e91e49dfaada07c94394c7422f431fbae52bed80
Subham Das <dassubham919@gmail.com>
2024-09-16 10:29:58 +0545

    General Formula for Patterns

--- C++ with DSA/5. Patterns/InvertedStar.cpp
+++ C++ with DSA/5. Patterns/InvertedStar.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 int main(){
-    int n = 4;
+    int n = 5;
     int i,j;
 
-    for (i=n; i>=1; i--){
-        for (j=i; j>=1; j--){
+    // for (i=n; i>=1; i--){
+    //     for (j=i; j>=1; j--){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    for (i=1; i<=n; i++){
+        for (j=1; j<=(n-i+1); j++){
             cout << "*";
         }
         cout << endl;

5febf2282922ff477ef7c139ec158a684c829bb6
Subham Das <dassubham919@gmail.com>
2024-11-28 19:44:56 +0545

    Defining an array

---
+++ C++ with DSA/8. Array/code.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marks[5] = {99,100,88,54,66};
+
+    cout << marks[0] << endl; 
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
+    return 0;
+}

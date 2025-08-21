451f04393553e92c061a03ae4ff7daa4412edaa2
Subham Das <dassubham919@gmail.com>
2025-08-21 13:42:33 +0530

    Traversing of Array

---
+++ C++ with DSA/Questions/LAB/3Lab1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A[100]={5,8,3,9,2,7,1,6,4,10};         //Traversing of Array
+    int n=10;
+    for (int i=0; i<n; i++){
+        cout << A[i] << " ";
+    }
+    return 0;
+}

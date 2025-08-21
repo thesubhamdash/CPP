980e0e1bd29072bb42c27e5b50e97acf9f949ca2
Subham Das <dassubham919@gmail.com>
2025-08-21 13:35:06 +0530

    Folder Maintained

---
+++ C++ with DSA/Questions/pattern1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,i,j;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    for(i=0; i<n; i++){
+        
+    }
+    return 0;
+}

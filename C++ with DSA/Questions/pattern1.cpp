5c4000b474bda37d4bb7d5ff6cced6b4d4b5ca09
Subham Das <dassubham919@gmail.com>
2025-08-24 10:34:52 +0530

    pattern1

--- C++ with DSA/Questions/pattern1.cpp
+++ C++ with DSA/Questions/pattern1.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main(){
-    int n,i,j;
-    cout << "Enter the value of n: ";
-    cin >> n;
-
+    int n=3,i,j;
+    int num=1;
     for(i=0; i<n; i++){
-        
+        for(j=0; j<n; j++){
+            cout << num << " ";
+            num += 1;
+        }
+        cout << endl;
     }
     return 0;
 }

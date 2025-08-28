8f450c1730088e9edfa0755c60b554be2771429e
Subham Das <dassubham919@gmail.com>
2025-08-28 18:14:06 +0530

    Updated Pyramid Pattern

--- C++ with DSA/Questions/pattern11.cpp
+++ C++ with DSA/Questions/pattern11.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main(){
-    int i,j,n,num;
+    int i,j,n;
     cout << "Enter the value of n: ";
     cin >> n;
 
     for(i=0; i<n; i++){
-        num = 1;
         for(j=0; j<n-i-1; j++){
             cout << " ";
         }
-        for(j=0; j<=i; j++){
-            cout << num;
-            num += 1;
+        for(j=1; j<=i+1; j++){
+            cout << j;
         }
         if(i>0){
             for(j=i; j>0; j--){

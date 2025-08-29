872f983841d28017406758322c8d9498d3cf5e50
Subham Das <dassubham919@gmail.com>
2025-08-30 00:19:46 +0530

    Folder Rearranged

---
+++ C++ with DSA/Practice/pattern12.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,j,n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    // TOP
+    for(i=0; i<n; i++){
+            
+        // spaces
+        for(j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        cout << "*";
+
+        // inner spaces
+        if (i!=0){
+            for(j=0; j<2*i-1; j++){
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl; 
+    }
+
+    // BOTTOM
+    
+    for(i=0; i<n-1; i++){
+        // spaces
+        for(j=0; j<i+1; j++){
+            cout << " ";
+        }
+        cout << "*";
+        // inner spaces
+        if(i!=n-1){
+            for(j=i+1; j<; j++){
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}

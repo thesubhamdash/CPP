b30899338d451acb0e062ee3182f0127ea8a486d
Subham Das <dassubham919@gmail.com>
2025-10-04 10:40:14 +0530

    Printed All possible Subarrays in an array

---
+++ C++ with DSA/8. Array/PrintSubarrays.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    int n=6;
+
+    for(int st=0; st<n; st++){
+        for(int end=st; end<n; end++){
+            for(int i=st; i<=end; i++){
+                cout << arr[i] << " ";
+            }
+            cout << "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}

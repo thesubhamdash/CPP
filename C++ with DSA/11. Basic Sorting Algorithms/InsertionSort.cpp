8ae6f0fc64d9b9a27d003994e728d47a4dcc060f
thesubhamdash <dassubham919@gmail.com>
2026-06-12 15:32:53 +0545

    Shift based Insertion Sort

--- C++ with DSA/11. Basic Sorting Algorithms/InsertionSort.cpp
+++ C++ with DSA/11. Basic Sorting Algorithms/InsertionSort.cpp
@@ -6,7 +6,7 @@ void insertionSort(int *arr, int n){
         int curr = arr[i];
         int prev = i - 1;
         while(prev>=0 && arr[prev] > curr){
-            swap(arr[prev], arr[prev+1]);
+            arr[prev + 1] = arr[prev];
             prev--;
         }
         arr[prev+1] = curr;

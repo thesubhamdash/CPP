5f1c2a41c472e42b7ceeec9240c0ccfa3c838ddd
thesubhamdash <dassubham919@gmail.com>
2026-06-15 07:14:00 +0545

    Stair Case Search with another cell starting point

---
+++ C++ with DSA/12. 2D Arrays/StairCaseSearch2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+bool sortedmatrix(int mat[][4], int n, int m, int key){
+    int i=n-1, j=0, steps=0;
+
+    while(i<n && j>=0){
+        if(mat[i][j] == key){
+            cout << "Key found at (" << i << "," << j << ")" << endl;
+            cout << "steps= " << steps;
+            return true;
+        } else if(mat[i][j] < key){
+            j++;
+            steps++;
+        } else {
+            i--;
+            steps++;
+        }
+    }
+
+    cout << "Key not found!";
+    return false;
+}
+
+int main(){
+    int matrix[4][4] = {{10, 20, 30, 40}, 
+                        {15, 25, 35, 45},
+                        {27, 29, 37, 48},
+                        {32, 33, 39, 50}};
+
+    sortedmatrix(matrix, 4, 4, 35);
+    return 0;
+}

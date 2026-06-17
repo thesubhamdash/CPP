60ebe4a5b685453dfdf5af1d3122a070da96a981
thesubhamdash <dassubham919@gmail.com>
2026-06-17 17:20:25 +0545

    Strings in C++

---
+++ C++ with DSA/13. Strings/string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+    string str = "hello";
+    cout << str << endl;
+    str = "yellow";
+    cout << str << endl;
+    
+    return 0;
+}

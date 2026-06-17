123bb44daa521e15c14c83a1f30ed92d617c900e
thesubhamdash <dassubham919@gmail.com>
2026-06-17 17:25:54 +0545

    Taking a string as an input and accessing a full sentence as well as individual characters of the string.

--- C++ with DSA/13. Strings/string.cpp
+++ C++ with DSA/13. Strings/string.cpp
@@ -7,6 +7,15 @@ int main(){
     cout << str << endl;
     str = "yellow";
     cout << str << endl;
-    
+
+    string str1;
+    getline(cin, str1);
+
+    cout << str1 << endl;
+    cout << str1[0] << endl;
+    cout << str1[1] << endl;
+    cout << str1[2] << endl;
+    cout << str1[3] << endl;
+
     return 0;
 }

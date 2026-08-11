727de8de4b63c89f560f68a4169869262b4ec439
thesubhamdash <dassubham919@gmail.com>
2026-08-11 19:26:59 +0530

    Understood the concept of Shallow and Deep Copy in Default and Custom Copy Constructor

--- C++ with DSA/16. OOP/copyConstructor.cpp
+++ C++ with DSA/16. OOP/copyConstructor.cpp
@@ -5,10 +5,25 @@ class car{
     public:
         string name;
         string color;
+        int* mileage;
 
     car(string name, string color){
         this->name = name;
         this->color = color;
+        mileage = new int;  //Dynamic Allocation 
+        *mileage = 12;
+    }
+
+    car(car &og){
+        name = og.name;
+        color = og.color;
+            // Shallow Copy Constructor
+        // mileage = og.mileage;   // Only copies the address, and points to the same location in heap of previous object.
+       
+            //Deep Copy Constructor
+        mileage = new int;  //Dynamically allocated new memory for new object
+        *mileage = *og.mileage;     // Copies the original value to the newly allocated memory 
+
     }
 };
 
@@ -19,5 +34,11 @@ int main(){
     car c2(c1);
     cout << c2.name << endl;
     cout << c2.color << endl;
+    cout << *c2.mileage << endl;
+
+    *c2.mileage = 10;
+
+    cout << "C1 mileage: " << *c1.mileage << endl; 
+    cout << "C2 mileage: " << *c2.mileage << endl;
     return 0;
 }

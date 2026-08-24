72896384ab448a53e899f07a0bccef0a88ddd2e3
thesubhamdash <dassubham919@gmail.com>
2026-08-24 19:20:31 +0530

    Check if Array is sorted using recursion

---
+++ C++ with DSA/17. Recursion/checkArraySorted.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+bool isSorted(int arr[], int n, int i){
+    if(i == n-1){
+        return true;
+    }
+    if(arr[i] > arr[i+1]){
+        return false;
+    }
+    return isSorted(arr,n,i+1);
+}
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    cout << isSorted(arr,5,0);
+    return 0;
+}
